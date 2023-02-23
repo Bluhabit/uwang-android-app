d871af3ec9f2790f266cea6cae36e31eac8dcb34
Trian Damai <triannurizkillah@gmail.com>
2023-02-23 16:21:16 +0700

    feat: add listener event

--- androidApp/src/main/java/com/bluehabit/budgetku/android/base/EventListener.kt
+++ androidApp/src/main/java/com/bluehabit/budgetku/android/base/EventListener.kt
@@ -8,75 +8,72 @@ import com.bluehabit.budgetku.android.base.listener.SnackBarClickListener
 
 
 class EventListener {
-    private var appEvent: AppStateEventListener?=null
-    private var topAppBarListener: AppBarSelectedListener?=null
-    private var bottomAppBarListener: BottomAppBarClickListener?=null
-    private var snackBarClickListener: SnackBarClickListener?=null
-    private var bottomSheetListener: BottomSheetListener?=null
+    private var appEvent: AppStateEventListener? = null
+    private var topAppBarListener: AppBarSelectedListener? = null
+    private var bottomAppBarListener: BottomAppBarClickListener? = null
+    private var snackBarClickListener: SnackBarClickListener? = null
+    private var bottomSheetListener: BottomSheetListener? = null
 
 
-    fun addOnEventListener(
-        listener: AppStateEventListener
-    ){
+    fun addOnEventListener(listener: AppStateEventListener) {
         appEvent = listener
     }
-    fun sendEvent(eventName:String){
+
+    fun sendEvent(eventName: String) {
         appEvent?.onEvent(eventName)
     }
 
-    fun addOnAppBarListener(
-        listener: AppBarSelectedListener
-    ){
+    fun addOnAppBarListener(listener: AppBarSelectedListener) {
         topAppBarListener = listener
     }
 
-    fun topAppBarAction(id:String, params:Array<out String>){
+    fun topAppBarAction(id: String, params: Array<out String>) {
         topAppBarListener?.onAction(id, *params)
     }
-    fun topAppBarNavigation(){
+
+    fun topAppBarNavigation() {
         topAppBarListener?.onNavigation()
     }
 
-    fun addOnBottomAppBarListener(
-        listener: BottomAppBarClickListener
-    ){
+    fun addOnBottomAppBarListener(listener: BottomAppBarClickListener) {
         bottomAppBarListener = listener
     }
-    fun bottomAppBarClick(id:String,params: Array<out String>){
+
+    fun bottomAppBarClick(id: String, params: Array<out String>) {
         bottomAppBarListener?.onItemClick(
             id,
             *params
         )
     }
 
-    fun addSnackbarBarListener(
-        listener: SnackBarClickListener
-    ){
+    fun addSnackbarBarListener(listener: SnackBarClickListener) {
         snackBarClickListener = listener
     }
 
-    fun snackbarContent(id:String, params:Array<out String>){
+    fun snackbarContent(id: String, params: Array<out String>) {
         snackBarClickListener?.onContent(id, *params)
     }
-    fun snackbarAction(id:String){
+
+    fun snackbarAction(id: String) {
         snackBarClickListener?.onAction(id)
     }
 
-    fun addOnBottomSheetListener(listener: BottomSheetListener){
+    fun addOnBottomSheetListener(listener: BottomSheetListener) {
         bottomSheetListener = listener
     }
 
-    fun bottomSheetAction(id:String,params: Array<out String>){
-        bottomSheetListener?.onAction(id,*params)
+    fun bottomSheetAction(id: String, params: Array<out String>) {
+        bottomSheetListener?.onAction(id, *params)
     }
 
-    fun bottomSheetContent(id: String,params: Array<out String>){
+    fun bottomSheetContent(id: String, params: Array<out String>) {
         bottomSheetListener?.onContentClick(
             id,
             *params
         )
     }
-    fun bottomSheetClose(){
+
+    fun bottomSheetClose() {
         bottomSheetListener?.onClose()
     }
 }
