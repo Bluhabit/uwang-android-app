3cd71a70dda76e782009b83d45664608be69e9db
IBM Trian Damai <triannurizkillah@gmail.com>
2023-03-18 04:01:24 +0700

    feat: update architecture

--- androidApp/src/main/java/com/bluehabit/budgetku/android/feature/signIn/Route.kt
+++ androidApp/src/main/java/com/bluehabit/budgetku/android/feature/signIn/Route.kt
@@ -8,7 +8,13 @@
 package com.bluehabit.budgetku.android.feature.signIn
 
 import androidx.activity.compose.rememberLauncherForActivityResult
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Button
 import androidx.compose.material.Text
 import androidx.compose.material.TextField
