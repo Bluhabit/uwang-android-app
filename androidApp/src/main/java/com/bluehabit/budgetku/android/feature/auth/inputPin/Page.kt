9f5b32e5a2d262bdfbc835870e9361c56977c758
masherrs <herrysusanto326@gmail.com>
2023-04-02 23:50:54 +0700

    Menambahkan file di dalam package inputPin

--- androidApp/src/main/java/com/bluehabit/budgetku/android/feature/auth/inputPin/Page.kt
+++ androidApp/src/main/java/com/bluehabit/budgetku/android/feature/auth/inputPin/Page.kt
@@ -7,3 +7,40 @@
 
 package com.bluehabit.budgetku.android.feature.auth.inputPin
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.NavGraphBuilder
+import androidx.navigation.compose.composable
+import com.bluehabit.budgetku.android.ApplicationState
+import com.bluehabit.budgetku.android.base.BaseMainApp
+import com.bluehabit.budgetku.android.base.UIWrapper
+
+object InputPin {
+    const val routeName = "InputPin"
+}
+
+fun NavGraphBuilder.routeInputPin(
+    state: ApplicationState,
+) {
+    composable(InputPin.routeName) {
+        ScreenInputPin(appState = state)
+    }
+}
+
+@Composable
+internal fun ScreenInputPin(
+    appState: ApplicationState,
+) = UIWrapper<InputPinViewModel>(appState = appState) {
+    Column {
+
+    }
+}
+
+@Preview
+@Composable
+fun PreviewScreenInputPin() {
+    BaseMainApp {
+        ScreenInputPin(it)
+    }
+}
