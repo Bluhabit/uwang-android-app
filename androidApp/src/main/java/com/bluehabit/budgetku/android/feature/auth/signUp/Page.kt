0f6eca2041e6d4980852f1892c7d39f9f9f501bd
WAHYU <wahyurenaldi27@gmail.com>
2023-04-09 01:10:55 +0700

    progres signup

--- androidApp/src/main/java/com/bluehabit/budgetku/android/feature/auth/signUp/Page.kt
+++ androidApp/src/main/java/com/bluehabit/budgetku/android/feature/auth/signUp/Page.kt
@@ -35,12 +35,10 @@ import com.bluehabit.budgetku.android.base.BaseMainApp
 import com.bluehabit.budgetku.android.base.UIWrapper
 import com.bluehabit.budgetku.android.base.contract.GoogleAuthContract
 import com.bluehabit.budgetku.android.base.extensions.navigateAndReplaceAll
-import com.bluehabit.budgetku.android.components.AnnotationTextItem
-import com.bluehabit.budgetku.android.components.FormInput
-import com.bluehabit.budgetku.android.components.IconWithAction
-import com.bluehabit.budgetku.android.components.TextWithAction
+import com.bluehabit.budgetku.android.components.*
 import com.bluehabit.budgetku.android.feature.auth.signIn.SignIn
 import com.bluehabit.budgetku.android.rememberApplicationState
+import com.bluehabit.budgetku.android.ui.Grey700
 
 
 object SignUp {
@@ -59,87 +57,92 @@ fun NavGraphBuilder.routeSignUp(
 @Composable
 internal fun ScreenSignUp(
     appState: ApplicationState
-) =UIWrapper<SignUpViewModel>(appState = appState) {
-        val state by uiState.collectAsState()
+) = UIWrapper<SignUpViewModel>(appState = appState) {
+    val state by uiState.collectAsState()
 
-        val launcher = rememberLauncherForActivityResult(
-            contract = GoogleAuthContract(),
-            onResult = { dispatch(SignUpEvent.SignUpWithGoogle(it)) }
-        )
+    val launcher = rememberLauncherForActivityResult(
+        contract = GoogleAuthContract(),
+        onResult = { dispatch(SignUpEvent.SignUpWithGoogle(it)) }
+    )
 
 
     Column(
-        modifier = Modifier.fillMaxSize()
+        modifier = Modifier
             .fillMaxSize()
             .padding(
                 start = 20.dp,
-                end = 20.dp ,
-                top =  20.dp,
+                end = 20.dp,
+                top = 20.dp,
+                bottom = 32.dp,
             ),
-    ) {
-        Image(
-            painter = painterResource(R.drawable.logo_budgetku_full),
-            contentDescription = "Logo Budgetku Full",
-                    modifier = Modifier.size(169.dp)
-        )
+        verticalArrangement = Arrangement.SpaceBetween,
 
-        Text(
-            text = "Daftar Akun Baru",
-            style = MaterialTheme.typography.h4,
-            fontWeight = FontWeight.Bold
-        )
-        Spacer(modifier = Modifier.height(20.dp))
-        Row(
-            verticalAlignment = Alignment.CenterVertically,
-            horizontalArrangement = Arrangement.SpaceBetween,
         ) {
-            // Teks "Sudah punya akun?"
-            Text(
+        Column() {
+            Image(
+                painter = painterResource(R.drawable.logo_budgetku_full),
+                contentDescription = "Logo Budgetku Full",
+                modifier = Modifier.size(169.dp)
+            )
 
-                text = "Sudah punya akun?",
-                color = Color.Black,
-                style = MaterialTheme.typography.subtitle1
-            )
-            // Teks "Login disini" yang bisa diklik
             Text(
-                text = "Login disini",
-                color = MaterialTheme.colors.primary,
-                style = MaterialTheme.typography.subtitle1,
-                fontWeight = FontWeight.Bold,
-                modifier = Modifier.clickable {
-                    // Aksi yang dilakukan ketika teks "Login disini" diklik
-                    // Contoh: Navigasi ke halaman login
-                }
+                text = "Daftar Akun Baru",
+                style = MaterialTheme.typography.h4,
+                fontWeight = FontWeight.Bold
             )
+            Spacer(modifier = Modifier.height(40.dp))
+            Row(
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.SpaceBetween,
+                modifier = Modifier.fillMaxWidth()
+            ) {
+                // Teks "Sudah punya akun?"
+                Text(
+
+                    text = "Sudah punya akun?",
+                    color = Grey700,
+                    style = MaterialTheme.typography.subtitle1
+                )
+                // Teks "Login disini" yang bisa diklik
+                Text(
+                    text = "Login disini",
+                    color = MaterialTheme.colors.primary,
+                    style = MaterialTheme.typography.subtitle1,
+                    fontWeight = FontWeight.Bold,
+                    modifier = Modifier.clickable {
+                        // Aksi yang dilakukan ketika teks "Login disini" diklik
+                        // Contoh: Navigasi ke halaman login
+                    }
+                )
+            }
         }
 
         Column(
             modifier = Modifier
-                .fillMaxWidth()
-                .fillMaxHeight()
-                .padding(
-                    horizontal = 16.dp
-                ),
+                .fillMaxWidth(),
             horizontalAlignment = Alignment.CenterHorizontally,
             verticalArrangement = Arrangement.Center
         ) {
-                FormInput(
-                    value = state.fullName,
-                    label = "Email",
-                    placeholder = "Masukkan Email Kamu",
-                    onChange = {
-                        commit {
-                            copy(
-                                fullName = it
-                            )
-                        }
-                    },
-                    keyboardOptions = KeyboardOptions(
-                        imeAction = ImeAction.Next
-                    ),
-                )
+            FormInput(
+                value = state.fullName,
+                label = "Email",
+                placeholder = "Masukkan Email Kamu",
+                onChange = {
+                    commit {
+                        copy(
+                            fullName = it
+                        )
+                    }
+                },
+                keyboardOptions = KeyboardOptions(
+                    imeAction = ImeAction.Next
+                ),
+            )
+            Spacer(modifier = Modifier.height(16.dp))
             Row(
-                verticalAlignment = Alignment.End,
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.End,
+                modifier = Modifier.fillMaxWidth()
             ) {
                 Text(
                     text = "Lupa Password?",
@@ -153,35 +156,33 @@ internal fun ScreenSignUp(
                 )
             }
 
-                    Spacer(modifier = Modifier.height(16.dp))
-                Button(
-                    onClick = {
-                        dispatch(SignUpEvent.SignUpWithEmail)
-                    },
-                    modifier = Modifier.fillMaxWidth()
-                ) {
-                    Text(text = "Daftar")
-                }
+            Spacer(modifier = Modifier.height(40.dp))
+            ButtonPrimary(
+                text = "Daftar",
+                onClick = { launcher.launch(1) },
+            )
+            Spacer(modifier = Modifier.height(40.dp))
             Row(
                 verticalAlignment = Alignment.CenterVertically,
                 horizontalArrangement = Arrangement.Center,
             ) {
                 Text(
                     text = "Atau",
-                    color = MaterialTheme.colors.,
+                    color = Grey700,
                     style = MaterialTheme.typography.subtitle1
                 )
             }
-                Button(
-                    onClick = { launcher.launch(1) },
-                    modifier = Modifier.fillMaxWidth()
-                ) {
-                    Text(text = "Daftar dengan Google")
-                }
+            Spacer(modifier = Modifier.height(20.dp))
+            Button(
+                onClick = { launcher.launch(1) },
+                modifier = Modifier.fillMaxWidth()
+            ) {
+                Text(text = "Daftar dengan Google")
             }
-            Spacer(modifier = Modifier.height(16.dp))
+        }
+        Spacer(modifier = Modifier.height(10.dp))
         IconWithAction(
-            icon={
+            icon = {
                 Icon(imageVector = Icons.Outlined.Info, contentDescription = "")
             },
             labels = listOf(
@@ -192,10 +193,8 @@ internal fun ScreenSignUp(
             )
         )
 
-        }
     }
-
-
+}
 
 
 @Preview
