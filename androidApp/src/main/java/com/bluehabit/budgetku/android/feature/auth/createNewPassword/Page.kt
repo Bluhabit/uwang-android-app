c30b9641d1ac4ff7206a8184700a02ad411413ee
achmadss <anakinskywalk1@gmail.com>
2023-04-12 07:24:11 +0700

    feat: create new password

---
+++ androidApp/src/main/java/com/bluehabit/budgetku/android/feature/auth/createNewPassword/Page.kt
@@ -0,0 +1,174 @@
+/*
+ * Copyright © 2023 Blue Habit.
+ *
+ * Unauthorized copying, publishing of this file, via any medium is strictly prohibited
+ * Proprietary and confidential
+ */
+
+package com.bluehabit.budgetku.android.feature.auth.createNewPassword
+
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.ArrowBack
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavGraphBuilder
+import androidx.navigation.compose.composable
+import com.bluehabit.budgetku.android.ApplicationState
+import com.bluehabit.budgetku.android.R
+import com.bluehabit.budgetku.android.base.BaseMainApp
+import com.bluehabit.budgetku.android.base.UIWrapper
+import com.bluehabit.budgetku.android.components.ButtonPrimary
+import com.bluehabit.budgetku.android.components.FormInputPassword
+import com.bluehabit.budgetku.android.rememberApplicationState
+
+object CreateNewPassword {
+    const val routeName = "CreateNewPassword"
+}
+
+fun NavGraphBuilder.routeCreateNewPassword(
+    state: ApplicationState,
+) {
+    composable(CreateNewPassword.routeName) {
+        ScreenCreateNewPassword(
+            state
+        )
+    }
+}
+
+@Composable
+internal fun ScreenCreateNewPassword(appState: ApplicationState) = UIWrapper<ResetPasswordViewModel>(
+    appState = appState
+) {
+    val state by uiState.collectAsState()
+    with(appState) {
+        setupTopAppBar {
+            TopAppBar(
+                navigationIcon = {
+                    Icon(
+                        imageVector = Icons.Outlined.ArrowBack,
+                        contentDescription = "",
+                        modifier = Modifier
+                            .clickable {
+                                navigateUp()
+                            }
+                    )
+                },
+                backgroundColor = Color.White,
+                title = {
+                    Text(text = "")
+                },
+            )
+        }
+    }
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(
+                start = 20.dp,
+                end = 20.dp,
+                bottom = 24.dp
+            ),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.SpaceBetween
+    ) {
+        with(appState) {
+            setupTopAppBar {
+                TopAppBar(
+                    modifier = Modifier
+                        .padding(horizontal = 12.dp),
+                    elevation = 0.dp,
+                    navigationIcon = {
+                        Icon(
+                            imageVector = Icons.Outlined.ArrowBack,
+                            contentDescription = "",
+                            modifier = Modifier
+                                .clickable {
+                                    navigateUp()
+                                }
+                        )
+                    },
+                    backgroundColor = Color.White,
+                    title = {
+                        Text(text = "")
+                    },
+                )
+            }
+        }
+        Column(
+            modifier = Modifier,
+            verticalArrangement = Arrangement.spacedBy(16.dp),
+            horizontalAlignment = Alignment.Start
+        ) {
+            Text(
+                text = stringResource(id = R.string.title_create_new_password),
+                style = MaterialTheme.typography.h5,
+                fontWeight = FontWeight.Bold,
+                color = Color(0xFF212121),
+            )
+            Text(
+                modifier = Modifier.padding(bottom = 5.dp),
+                text = stringResource(id = R.string.detail_create_new_password),
+                style = MaterialTheme.typography.subtitle1,
+                fontWeight = FontWeight.Normal,
+                color = Color(0xFF616161),
+            )
+            FormInputPassword(
+                value = state.password,
+                label = stringResource(id = R.string.label_input_password_create_new_password),
+                placeholder = stringResource(id = R.string.placeholder_password_create_new_password),
+                error = state.isInputPasswordError,
+                errorMessage = state.errorInputPassword,
+                onChange = {
+                    commit { copy(password = it) }
+                    dispatch(CreateNewPasswordEvent.ValidatePassword)
+                    dispatch(CreateNewPasswordEvent.ValidateConfirmPassword)
+                    dispatch(CreateNewPasswordEvent.HandleButtonSaveChanges)
+                }
+            )
+            FormInputPassword(
+                value = state.confirmPassword,
+                label = stringResource(id = R.string.label_confirm_password_create_new_password),
+                placeholder = stringResource(id = R.string.placeholder_password_create_new_password),
+                error = state.isConfirmPasswordError,
+                errorMessage = state.errorConfirmPassword,
+                onChange = {
+                    commit { copy(confirmPassword = it) }
+                    dispatch(CreateNewPasswordEvent.ValidatePassword)
+                    dispatch(CreateNewPasswordEvent.ValidateConfirmPassword)
+                    dispatch(CreateNewPasswordEvent.HandleButtonSaveChanges)
+                }
+            )
+        }
+
+        ButtonPrimary(
+            text = stringResource(id = R.string.text_button_save_new_password),
+            enabled = state.isButtonEnabled,
+            onClick = { dispatch(CreateNewPasswordEvent.ChangeToNewPassword(state.password)) }
+        )
+
+    }
+
+}
+
+
+@Preview
+@Composable
+fun PreviewScreenSignIn() {
+    BaseMainApp {
+        ScreenCreateNewPassword(
+            appState = rememberApplicationState()
+        )
+    }
+}
+
---
+++ androidApp/src/main/java/com/bluehabit/budgetku/android/feature/auth/createNewPassword/ViewModel.kt
@@ -0,0 +1,80 @@
+/*
+ * Copyright © 2023 Blue Habit.
+ *
+ * Unauthorized copying, publishing of this file, via any medium is strictly prohibited
+ * Proprietary and confidential
+ */
+
+package com.bluehabit.budgetku.android.feature.auth.createNewPassword
+
+import com.bluehabit.budgetku.android.R
+import com.bluehabit.budgetku.android.base.BaseViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class ResetPasswordViewModel @Inject constructor(
+
+) : BaseViewModel<CreateNewPasswordState, CreateNewPasswordEvent>(CreateNewPasswordState()) {
+
+    init {
+        handleActions()
+    }
+
+    private fun validateInputPassword(
+        valid: suspend (Boolean, String) -> Unit
+    ) = asyncWithState {
+        when {
+            password.isEmpty() -> valid(false, getString(R.string.subtitle_error_password_empty))
+            password.length < 8 -> valid(false, getString(R.string.subtitle_error_password_length))
+            else -> valid(true, "")
+        }
+    }
+
+    private fun validateConfirmPassword(
+        valid: suspend (Boolean, String) -> Unit
+    ) = asyncWithState {
+        when {
+            confirmPassword != password -> valid(false, getString(R.string.subtitle_error_password_not_identical))
+            confirmPassword.isEmpty() -> valid(false, getString(R.string.subtitle_error_password_empty))
+            else -> valid(true, "")
+        }
+    }
+
+    private fun handleButtonSave(
+        callback: suspend (Boolean) -> Unit
+    ) = asyncWithState {
+        when {
+            !isInputPasswordError && !isConfirmPasswordError -> callback(true)
+        }
+    }
+
+    override fun handleActions() = onEvent {
+        when (it) {
+            is CreateNewPasswordEvent.ValidatePassword -> validateInputPassword { isValid, errorMessage ->
+                commit {
+                    copy(
+                        isInputPasswordError = !isValid,
+                        errorInputPassword = errorMessage,
+                    )
+                }
+            }
+            is CreateNewPasswordEvent.ValidateConfirmPassword -> validateConfirmPassword { isValid, errorMessage ->
+                commit {
+                    copy(
+                        isConfirmPasswordError = !isValid,
+                        errorConfirmPassword = errorMessage,
+                    )
+                }
+            }
+            is CreateNewPasswordEvent.HandleButtonSaveChanges -> handleButtonSave {
+                commit { copy(isButtonEnabled = it) }
+            }
+            is CreateNewPasswordEvent.ChangeToNewPassword -> {
+                showSnackbar("Change password to ${it.password}")
+            }
+        }
+    }
+}
+
+
