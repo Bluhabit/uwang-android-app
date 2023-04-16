f56e7e410d15bbf649913d5f7ad98aa17e0c39f2
WAHYU <wahyurenaldi27@gmail.com>
2023-04-16 15:45:19 +0700

    progres verification email sign up

--- androidApp/src/main/java/com/bluehabit/budgetku/android/components/ContentBottomSheetSignUp.kt
+++ androidApp/src/main/java/com/bluehabit/budgetku/android/components/ContentBottomSheetSignUp.kt
@@ -20,18 +20,15 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.SpanStyle
 import androidx.compose.ui.text.buildAnnotatedString
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.bluehabit.budgetku.android.R
 import com.bluehabit.budgetku.android.base.BaseMainApp
-import com.bluehabit.budgetku.android.ui.Grey700
 import com.bluehabit.budgetku.android.ui.Grey900
 
 @Composable
