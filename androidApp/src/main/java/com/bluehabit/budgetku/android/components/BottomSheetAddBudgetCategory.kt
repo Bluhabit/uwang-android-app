d3dae991b091c9ad98e8495ac4910cdab7a41a24
achmadss <anakinskywalk1@gmail.com>
2023-04-17 23:08:39 +0700

    refactor: removed wild imports

--- androidApp/src/main/java/com/bluehabit/budgetku/android/components/BottomSheetAddBudgetCategory.kt
+++ androidApp/src/main/java/com/bluehabit/budgetku/android/components/BottomSheetAddBudgetCategory.kt
@@ -11,7 +11,15 @@ import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CircleShape
