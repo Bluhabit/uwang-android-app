65f14620da02c314668c6971680e6228ead65d51
IBM Trian Damai <triannurizkillah@gmail.com>
2023-04-22 00:01:48 +0700

    feat: update list saving

--- androidApp/src/main/java/com/bluehabit/budgetku/android/components/CardItemCategory.kt
+++ androidApp/src/main/java/com/bluehabit/budgetku/android/components/CardItemCategory.kt
@@ -30,7 +30,6 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.bluehabit.budgetku.android.R
 import com.bluehabit.budgetku.android.base.BaseMainApp
-import com.bluehabit.budgetku.android.ui.Grey100
 import com.bluehabit.budgetku.android.ui.Grey200
 import com.bluehabit.budgetku.android.ui.Grey300
 import com.bluehabit.budgetku.android.ui.Grey900
