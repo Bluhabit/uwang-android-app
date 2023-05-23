55bf4278ebd365bd65a68a53b563359d6cd81c48
trian damai <triannurizkillah@gmail.com>
2023-05-24 00:22:04 +0700

    finishing for pikmi

--- androidApp/src/main/java/com/bluehabit/budgetku/android/components/ItemArticleGrid.kt
+++ androidApp/src/main/java/com/bluehabit/budgetku/android/components/ItemArticleGrid.kt
@@ -32,7 +32,6 @@ import androidx.compose.ui.draw.clip
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalDensity
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
