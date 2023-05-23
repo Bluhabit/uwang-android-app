90053d88b926ac0852880f921c97481cd1f7bb55
trian damai <triannurizkillah@gmail.com>
2023-05-24 00:47:18 +0700

    finishing for pikmi

--- data/src/main/java/com/bluehabit/budgetku/data/remote/dummy/DummyArticles.kt
+++ data/src/main/java/com/bluehabit/budgetku/data/remote/dummy/DummyArticles.kt
@@ -13,28 +13,28 @@ val dummyArticles: List<ArticleModel> = listOf(
     ArticleModel(
         title = "Pusat Bantuan",
         body = "Punya kendala atau pertanyyan terkait Budgetku? kamu bisa kirim di sini",
-        image = "https://github.com/Blue-Habit/budgetku-mobile-app/blob/feature/BKA-131/assets/dummy_article_community_1.webp",
+        image = "https://rawcdn.githack.com/triandamai/Pokedex/5750f43f13078517e2b97514f336ca05eee298a4/assets/dummy_article_community_1.webp",
         date = "11 April 2023, 22:54",
         likes = 200
     ),
     ArticleModel(
         title = "Cerdas Finansial",
         body = "Yuk melek finansial bersama Budgetku. Tersedia course keuangan untukmu",
-        image = "https://github.com/Blue-Habit/budgetku-mobile-app/blob/feature/BKA-131/assets/dummy_article_community_2.webp",
+        image = "https://rawcdn.githack.com/triandamai/Pokedex/5750f43f13078517e2b97514f336ca05eee298a4/assets/dummy_article_community_2.webp",
         date = "11 April 2023, 22:54",
         likes = 300
     ),
     ArticleModel(
         title = "Promo & Hadiah",
         body = "Yuk cek berbagai promo menarik di aplikasi Budgetku Sekarang!",
-        image = "https://github.com/Blue-Habit/budgetku-mobile-app/blob/feature/BKA-131/assets/dummy_article_community_3.webp",
+        image = "https://rawcdn.githack.com/triandamai/Pokedex/5750f43f13078517e2b97514f336ca05eee298a4/assets/dummy_article_community_3.webp",
         date = "11 April 2023, 22:54",
         likes = 10
     ),
     ArticleModel(
         title = "Tips Keuangan",
         body = "Bingung ngatur budget? disini kamu bisa tahu tips ngatur keuangan",
-        image = "https://github.com/Blue-Habit/budgetku-mobile-app/blob/feature/BKA-131/assets/dummy_article_community_4.webp",
+        image = "https://rawcdn.githack.com/triandamai/Pokedex/5750f43f13078517e2b97514f336ca05eee298a4/assets/dummy_article_community_4.webp",
         date = "11 April 2023, 22:54",
         likes = 355
     )
