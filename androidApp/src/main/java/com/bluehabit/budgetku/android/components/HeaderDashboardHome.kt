259a8272483cffce5332dffb4add6bd2a28c002a
trian damai <triannurizkillah@gmail.com>
2023-05-24 07:56:19 +0700

    finishing for pikmi

--- androidApp/src/main/java/com/bluehabit/budgetku/android/components/HeaderDashboardHome.kt
+++ androidApp/src/main/java/com/bluehabit/budgetku/android/components/HeaderDashboardHome.kt
@@ -30,7 +30,6 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
