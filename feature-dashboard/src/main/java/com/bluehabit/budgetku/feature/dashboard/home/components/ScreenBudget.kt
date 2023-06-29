6d45ab1b440e890037666459f05b7eda06fe2efb
trian damai <triannurizkillah@gmail.com>
2023-06-29 23:05:16 +0700

    modularize app

--- feature-dashboard/src/main/java/com/bluehabit/budgetku/feature/dashboard/home/components/ScreenBudget.kt
+++ feature-dashboard/src/main/java/com/bluehabit/budgetku/feature/dashboard/home/components/ScreenBudget.kt
@@ -35,7 +35,6 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import com.bluehabit.budgetku.feature.dashboard.budget.BottomSheetBudget
 import com.bluehabit.budgetku.feature.dashboard.component.CardEmptyBudget
 import com.bluehabit.budgetku.feature.dashboard.component.CardEmptyTransactionBudget
 import com.bluehabit.budgetku.feature.dashboard.component.CardSummaryBudget
