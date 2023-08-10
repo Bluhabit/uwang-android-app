37f8ae789fdaea23ff81ad7df4bce607bf9324d3
trian damai <triannurizkillah@gmail.com>
2023-08-10 14:05:18 +0700

    feat: create business logic sign-in,sign-up,otp-confirmation
    jira refs:
    https://bluehabit.atlassian.net/browse/GAWE-39
    https://bluehabit.atlassian.net/browse/GAWE-44
    https://bluehabit.atlassian.net/browse/GAWE-46

---
+++ data/data-authentication/src/main/java/com/bluehabit/eureka/data/authentication/datasource/remote/response/UserInfoResponse.kt
@@ -0,0 +1,12 @@
+/*
+ * Copyright © 2023 Blue Habit.
+ *
+ * Unauthorized copying, publishing of this file, via any medium is strictly prohibited
+ * Proprietary and confidential
+ */
+
+package com.bluehabit.eureka.data.authentication.datasource.remote.response
+
+data class UserInfoResponse (
+    val fullName: String
+)
