6af57b8e27ff68d0fc46f80ef3d5d2146dc5b540
azkifairuz <azkiajmal@gmail.com>
2023-08-11 11:49:02 +0700

    fix: refactor file name

---
+++ data/data-authentication/src/main/java/com/bluehabit/eureka/data/authentication/datasource/remote/request/SetNewPasswordRequest.kt
@@ -0,0 +1,12 @@
+/*
+ * Copyright © 2023 Blue Habit.
+ *
+ * Unauthorized copying, publishing of this file, via any medium is strictly prohibited
+ * Proprietary and confidential
+ */
+
+package com.bluehabit.eureka.data.authentication.datasource.remote.request
+
+data class SetNewPasswordRequest (
+    val password: String,
+)
