a9830dda744db8573f38f617ed2edf05d889653a
trian damai <triannurizkillah@gmail.com>
2023-08-14 19:46:15 +0700

    feat: create screen,component input otp
    jira refs: GAWE-59,GAWE-60

--- core/core-component/src/main/java/com/bluehabit/core/ui/components/input/InputOtp.kt
+++ core/core-component/src/main/java/com/bluehabit/core/ui/components/input/InputOtp.kt
@@ -9,10 +9,13 @@ package com.bluehabit.core.ui.components.input
 
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.BasicTextField
@@ -22,8 +25,16 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.focus.FocusRequester
+import androidx.compose.ui.focus.focusRequester
+import androidx.compose.ui.focus.onFocusChanged
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.TextRange
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.input.TextFieldValue
@@ -31,8 +42,10 @@ import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import app.trian.mvi.ui.extensions.Empty
+import app.trian.mvi.ui.extensions.from
 import com.bluehabit.core.ui.theme.GaweanTheme
-import java.lang.IllegalArgumentException
+import com.bluehabit.core.ui.theme.Gray300
+import com.bluehabit.core.ui.theme.Gray900
 
 @Composable
 fun InputOtp(
@@ -41,15 +54,18 @@ fun InputOtp(
     length: Int = 4,
     onChange: (String, Boolean) -> Unit = { _, _ -> }
 ) {
-    LaunchedEffect(key1 = otp, block = {
-        if (otp.length > length) {
-            throw IllegalArgumentException(
-                "Otp text value must not have more than otpLength: $length characters"
-            )
-        }
+    val focusRequester = remember { FocusRequester() }
+    var isFocused by remember { mutableStateOf(false) }
+
+    LaunchedEffect(key1 = Unit, block = {
+        focusRequester.requestFocus()
     })
     BasicTextField(
-        modifier = modifier,
+        modifier = modifier
+            .focusRequester(focusRequester)
+            .onFocusChanged {
+                isFocused = it.isFocused
+            },
         value = TextFieldValue(
             otp,
             selection = TextRange(otp.length)
@@ -72,7 +88,11 @@ fun InputOtp(
                 horizontalArrangement = Arrangement.Center
             ) {
                 repeat(length) {
-                    CharView(index = it, text = otp)
+                    CharView(
+                        index = it,
+                        text = otp,
+                        isFocused = isFocused
+                    )
                     Spacer(modifier = Modifier.width(8.dp))
                 }
 
@@ -84,33 +104,38 @@ fun InputOtp(
 @Composable
 fun CharView(
     index: Int,
-    text: String
+    text: String,
+    isFocused: Boolean = false
 ) {
-    val isFocused = text.length == index
+    val context = LocalContext.current
     val char = when {
         index == text.length -> "0"
-        index > text.length -> ""
+        index > text.length -> "0"
         else -> text[index].toString()
     }
-    Text(
+    Box(
         modifier = Modifier
-            .width(40.dp)
+            .size(64.dp.from(context = context))
             .border(
                 width = 1.dp,
                 color = when {
-                    isFocused -> Color.Green
-                    else -> Color.LightGray
+                    isFocused -> Gray900
+                    else -> Gray300
                 },
                 shape = RoundedCornerShape(8.dp)
-            ),
-        text = char,
-        style = MaterialTheme.typography.h4,
-        color = if (isFocused) Color.Green else Color.LightGray,
-        textAlign = TextAlign.Center
-    )
+            )
+    ) {
+        Text(
+            modifier = Modifier.align(Alignment.Center),
+            text = char,
+            style = MaterialTheme.typography.h4,
+            color = if (isFocused) Gray900 else Gray300,
+            textAlign = TextAlign.Center
+        )
+    }
 }
 
-@Preview
+@Preview(showBackground = true, backgroundColor = 0xFFFFFFFF)
 @Composable
 fun PreviewInputOtp() {
     GaweanTheme {
@@ -123,6 +148,11 @@ fun PreviewInputOtp() {
             InputOtp(
                 length = 4,
                 otp = "23456"
+            )
+            Spacer(modifier = Modifier.height(10.dp))
+            InputOtp(
+                length = 4,
+                otp = ""
             )
         }
     }
--- feature/feature-authentication/src/main/java/com/bluehabit/eureka/feature/authentication/signUp/screen/OtpScreen.kt
+++ feature/feature-authentication/src/main/java/com/bluehabit/eureka/feature/authentication/signUp/screen/OtpScreen.kt
@@ -7,9 +7,167 @@
 
 package com.bluehabit.eureka.feature.authentication.signUp.screen
 
+import android.util.Log
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Scaffold
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.withStyle
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import app.trian.mvi.ui.extensions.Empty
+import app.trian.mvi.ui.extensions.from
+import com.bluehabit.core.ui.R
+import com.bluehabit.core.ui.components.button.ButtonPrimary
+import com.bluehabit.core.ui.components.input.InputOtp
+import com.bluehabit.core.ui.theme.GaweanTheme
+import com.bluehabit.core.ui.theme.Primary600
+import com.bluehabit.core.ui.theme.Primary700
 
 @Composable
-fun OtpScreen() {
+fun OtpScreen(
+    modifier: Modifier = Modifier,
+    otp: String = String.Empty,
+    email: String = String.Empty,
+    onChange: (String) -> Unit = {}
+) {
+    val ctx = LocalContext.current
+    Scaffold(
+        bottomBar = {
+            Row(
+                modifier = modifier
+                    .fillMaxWidth()
+                    .padding(
+                        horizontal = 16.dp,
+                        vertical = 10.dp
+                    )
+            ) {
+                ButtonPrimary(
+                    modifier = modifier
+                        .fillMaxWidth(),
+                    text = stringResource(R.string.btn_confirmation_otp_screen),
+                    enabled = false
+                )
+            }
+        }
+    ) {
+        Column(
+            modifier = modifier
+                .padding(it)
+                .fillMaxSize()
+                .background(
+                    MaterialTheme.colors.surface
+                )
+                .padding(
+                    vertical = 20.dp,
+                    horizontal = 16.dp
+                ),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Top
+        ) {
+            Column(
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.SpaceBetween
+            ) {
+                Image(
+                    painter = painterResource(id = R.drawable.gawean_logo),
+                    contentDescription = stringResource(id = R.string.content_description_logo),
+                    modifier = modifier.height(34.dp.from(context = ctx))
+                )
+                Spacer(modifier = modifier.height(12.dp))
+                Text(
+                    text = stringResource(id = R.string.text_title_otp_screen),
+                    style = MaterialTheme.typography.body1,
+                    fontWeight = FontWeight.W600
+                )
+                Spacer(modifier = modifier.height(10.dp))
+                Text(
+                    text = stringResource(id = R.string.text_subtitle2_otp_screen),
+                    style = MaterialTheme.typography.body1,
+                    fontWeight = FontWeight.W400
+                )
+                Text(text = buildAnnotatedString {
+                    withStyle(
+                        style = SpanStyle(
+                            fontStyle = MaterialTheme.typography.body1.fontStyle,
+                            fontWeight = FontWeight.W400
+                        )
+                    ) {
+                        append(stringResource(id = R.string.text_subtitle3_otp_screen))
+                    }
+                    append(" ")
+                    withStyle(
+                        style = SpanStyle(
+                            color = Primary700,
+                            fontStyle = MaterialTheme.typography.body1.fontStyle,
+                            fontWeight = FontWeight.W400
+                        )
+                    ) {
+                        append(email)
+                    }
+                })
+                Spacer(modifier = modifier.height(20.dp))
+            }
+            Column(
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.SpaceBetween
+            ) {
+                InputOtp(
+                    otp = otp,
+                    onChange = { otp, valid ->
+                        if (valid) {
+                            onChange(otp)
+                        }
+                    }
+                )
+                Spacer(modifier = modifier.height(20.dp))
+                Text(
+                    text = stringResource(id = R.string.text_resend_otp),
+                    color = Primary600
+                )
+            }
+            Column {
 
+            }
+        }
+    }
+}
+
+@Preview
+@Composable
+fun PreviewOtpScreen() {
+    var otp by remember {
+        mutableStateOf("")
+    }
+    GaweanTheme {
+        OtpScreen(
+            otp = otp,
+            email="example@gmail.com",
+            onChange = {
+                otp = it
+            }
+        )
+    }
 }
