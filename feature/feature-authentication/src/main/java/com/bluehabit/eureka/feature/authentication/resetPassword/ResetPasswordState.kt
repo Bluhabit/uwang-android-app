4b5ce2878b8fa96a59f1086801fbe5c1cefe68db
fauzangraciaramadhani <fauzanramadhani06@gmail.com>
2023-08-16 12:01:44 +0700

    feat: rename InputNewPasswordScreen to CreateNewPasswordScreen file

--- feature/feature-authentication/src/main/java/com/bluehabit/eureka/feature/authentication/resetPassword/ResetPasswordState.kt
+++ feature/feature-authentication/src/main/java/com/bluehabit/eureka/feature/authentication/resetPassword/ResetPasswordState.kt
@@ -10,13 +10,15 @@ package com.bluehabit.eureka.feature.authentication.resetPassword
 import android.os.Parcelable
 import app.trian.mvi.ui.extensions.Empty
 import app.trian.mvi.ui.internal.contract.MviState
-import com.bluehabit.eureka.feature.authentication.resetPassword.ResetPasswordEffect
 import kotlinx.parcelize.Parcelize
 import kotlinx.parcelize.RawValue
 
 @Parcelize
 data class ResetPasswordState(
+    val currentScreen: Int = 1,
     val email: String = String.Empty,
+    val password: String = String.Empty,
+    val passwordConfirmation: String = String.Empty,
 
     override val effect: @RawValue ResetPasswordEffect = ResetPasswordEffect.Nothing
 ) : MviState<ResetPasswordEffect>(), Parcelable
