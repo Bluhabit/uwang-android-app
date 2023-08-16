e3536a185a5dbca2c8feb87e943029420b61bc0a
fauzangraciaramadhani <fauzanramadhani06@gmail.com>
2023-08-16 11:30:08 +0700

    feat: create InputNewPasswordScreen file

---
+++ feature/feature-authentication/src/main/java/com/bluehabit/eureka/feature/authentication/resetPassword/screen/InputNewPasswordScreen.kt
@@ -0,0 +1,108 @@
+/*
+ * Copyright © 2023 Blue Habit.
+ *
+ * Unauthorized copying, publishing of this file, via any medium is strictly prohibited
+ * Proprietary and confidential
+ */
+
+package com.bluehabit.eureka.feature.authentication.resetPassword.screen
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.withStyle
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import app.trian.mvi.ui.extensions.from
+import com.bluehabit.core.ui.R
+import com.bluehabit.core.ui.components.input.InputTextPrimary
+import com.bluehabit.core.ui.theme.GaweanTheme
+import com.bluehabit.core.ui.theme.Gray900
+import com.bluehabit.core.ui.theme.Primary700
+import com.bluehabit.eureka.feature.authentication.resetPassword.ResetPasswordState
+
+@Composable
+fun InputNewPasswordScreen(
+    modifier: Modifier = Modifier,
+    state: ResetPasswordState = ResetPasswordState(),
+    onEmailChanged: (String) -> Unit = {},
+    onResetPassword: () -> Unit = {}
+) {
+    val context = LocalContext.current
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color.White)
+            .padding(
+                vertical = 28.dp.from(context = context),
+                horizontal = 26.dp.from(context = context),
+            ),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.spacedBy(28.dp)
+    ) {
+        Image(
+            painter = painterResource(id = R.drawable.gawean_logo),
+            contentDescription = stringResource(id = R.string.content_description_logo),
+        )
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.spacedBy(4.dp),
+            modifier = Modifier
+                .fillMaxWidth()
+        ) {
+            Text(
+                text = stringResource(id = com.bluehabit.core.ui.R.string.text_title_instruction_send),
+                style = MaterialTheme.typography.h6,
+                lineHeight = 30.sp,
+                fontWeight = FontWeight.W600,
+                textAlign = TextAlign.Center,
+                color = Gray900
+            )
+            Text(
+                text = stringResource(id = com.bluehabit.core.ui.R.string.text_subtitle_instruction),
+                style = MaterialTheme.typography.body2,
+                lineHeight = 20.sp,
+                fontWeight = FontWeight.W400,
+                textAlign = TextAlign.Center,
+                color = Gray900
+            )
+        }
+
+        InputTextPrimary(
+            label = stringResource(id = R.string.text_label_input_password_screen_auth),
+            placeholder = stringResource(id = R.string.text_placeholder_input_password_screen_auth),
+            value = state.email,
+            onChange = onEmailChanged,
+            enable = true,
+        )
+    }
+}
+
+@Preview
+@Composable
+fun PreviewInputNewPassword() {
+    GaweanTheme {
+        InputNewPasswordScreen()
+    }
+
+}
