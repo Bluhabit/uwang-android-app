21f56610dd575f94e9d43c3c305372bfb75cf450
trian damai <triannurizkillah@gmail.com>
2023-08-18 14:00:54 +0700

    feat:  working on GAWE-2,GAWE-3,GAWE-25,GAWE-26

--- core/core-data/src/main/java/com/bluehabit/eureka/data/contract/GoogleAuthContract.kt
+++ core/core-data/src/main/java/com/bluehabit/eureka/data/contract/GoogleAuthContract.kt
@@ -19,7 +19,7 @@ import com.google.android.gms.tasks.Task
 class GoogleAuthContract: ActivityResultContract<Int, Task<GoogleSignInAccount>?>() {
     override fun createIntent(context: Context, input: Int): Intent {
         val gso= GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
-            .requestIdToken("949035954925-rd7v5t6tkla7i5ngbjntb95n4pqt6u7e.apps.googleusercontent.com")
+            .requestIdToken("264573731963-2434pg7hk30e0rp3h1852p1ehk1fdjs9.apps.googleusercontent.com")
             .requestProfile()
             .requestEmail()
             .build()
