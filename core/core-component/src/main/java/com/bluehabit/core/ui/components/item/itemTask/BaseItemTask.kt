5f4f74521307001078bab839562172d74280a224
fauzangraciaramadhani <fauzanramadhani06@gmail.com>
2023-08-23 14:31:18 +0700

    feat: Create CircleCheckBox component

---
+++ core/core-component/src/main/java/com/bluehabit/core/ui/components/item/itemTask/BaseItemTask.kt
@@ -0,0 +1,9 @@
+/*
+ * Copyright © 2023 Blue Habit.
+ *
+ * Unauthorized copying, publishing of this file, via any medium is strictly prohibited
+ * Proprietary and confidential
+ */
+
+package com.bluehabit.core.ui.components.item.itemTask
+
