cdc5f52f3f713e7949998821e8cafe1b9b62ec2b
WAHYU <wahyurenaldi27@gmail.com>
2023-08-26 15:48:07 +0700

    Create component input text area description task

--- core/core-component/src/main/java/com/bluehabit/core/ui/components/input/InputTextDescription.kt
+++ core/core-component/src/main/java/com/bluehabit/core/ui/components/input/InputTextDescription.kt
@@ -9,12 +9,12 @@ package com.bluehabit.core.ui.components.input
 
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.defaultMinSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.Icon
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.OutlinedTextField
 import androidx.compose.material.Text
@@ -25,29 +25,22 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import app.trian.mvi.ui.extensions.Empty
 import com.bluehabit.core.ui.R
-import com.bluehabit.core.ui.theme.Error500
+import com.bluehabit.core.ui.theme.Error300
+import com.bluehabit.core.ui.theme.Error600
 import com.bluehabit.core.ui.theme.GaweanTheme
 import com.bluehabit.core.ui.theme.Gray300
-import com.bluehabit.core.ui.theme.Gray500
+import com.bluehabit.core.ui.theme.Gray700
+import com.bluehabit.core.ui.theme.Gray900
 import com.bluehabit.core.ui.theme.Primary300
-import com.bluehabit.core.ui.theme.Rose700
-
-
-/**
- * Button Primary
- * @param modifier
- *
- * @sample PreviewInputTextPrimary
- * */
 
 @Composable
-fun InputTextPrimary(
+fun InputTextDescription(
     modifier: Modifier = Modifier,
     value: String = String.Empty,
     placeholder: String = String.Empty,
@@ -62,9 +55,9 @@ fun InputTextPrimary(
             text = label,
             style = MaterialTheme.typography.subtitle2,
             fontWeight = FontWeight.W500,
-            modifier = modifier
+            modifier = modifier,
+            color = Gray700
         )
-        Spacer(modifier = modifier.height(8.dp))
         OutlinedTextField(
             isError = error,
             enabled = enabled,
@@ -72,88 +65,78 @@ fun InputTextPrimary(
             onValueChange = onChange,
             colors = TextFieldDefaults.outlinedTextFieldColors(
                 placeholderColor = Gray300,
-                textColor = Gray500,
+                textColor = Gray900,
                 focusedBorderColor = Primary300,
                 disabledBorderColor = Gray300,
-                errorBorderColor = Rose700
+                errorBorderColor = Error600
             ),
-            modifier = modifier
+            modifier = Modifier
+                .width(320.dp)
+                .height(154.dp)
                 .fillMaxWidth()
-                .defaultMinSize(minHeight = 40.dp),
+                .heightIn(min = 200.dp),
             placeholder = {
                 Text(
                     text = placeholder,
                     style = MaterialTheme.typography.body1,
                     fontWeight = FontWeight.W400
                 )
+                Spacer(modifier = modifier.height(6.dp))
+                Text(
+                    text = errorMessage,
+                    style = MaterialTheme.typography.subtitle2,
+                    fontWeight = FontWeight.W400,
+                    modifier = modifier,
+                    color = Error300
+                )
             },
-            singleLine = true,
-            textStyle = MaterialTheme.typography.body1.copy(
-                fontWeight = FontWeight.W400
-            ),
-            shape = RoundedCornerShape(8.dp),
-            trailingIcon = if (error) {
-                {
-                    Icon(
-                        painter = painterResource(id = R.drawable.ic_info),
-                        contentDescription = "",
-                        tint = Error500
-                    )
-                }
-            } else null
-        )
-        Spacer(modifier = modifier.height(6.dp))
-        Text(
-            text = errorMessage,
-            style = MaterialTheme.typography.subtitle2,
-            fontWeight = FontWeight.W400,
-            modifier = modifier,
-            color = Error500
-        )
+
+            )
+
     }
 }
 
 @Preview(showBackground = true, backgroundColor = 0xFFFFFFFF)
 @Composable
-fun PreviewInputTextPrimary() {
-    GaweanTheme() {
+fun PreviewInputTextDescription() {
+    GaweanTheme()
+    {
         Column(
-            modifier = Modifier.padding(
-                horizontal = 20.dp,
-                vertical = 20.dp
-            )
+            modifier = Modifier
+                .padding(
+                    horizontal = 24.dp,
+                    vertical = 40.dp
+                )
+                .fillMaxWidth()
+                .heightIn(min = 215.dp),
         ) {
             var input by remember {
                 mutableStateOf("")
             }
-            InputTextPrimary(
-                label = "Email",
+            InputTextDescription(
                 value = input,
-                placeholder = "Masukkan email disini",
+                placeholder = stringResource(id = R.string.text_placeholder_input_description),
                 onChange = { input = it },
-                error = true
-            )
-            InputTextPrimary(
-                label = "Email",
-                value = input,
-                placeholder = "Masukkan email disini",
-                onChange = { input = it },
-                error = false
-            )
-            InputTextPrimary(
-                label = "Email",
-                value = input,
-                placeholder = "Masukkan email disini",
-                onChange = { input = it },
+                label = stringResource(id = R.string.text_label_input_description),
+                modifier = Modifier.size(
+                    width = 320.dp,
+                    height = 25.dp
+                ),
                 enabled = true
             )
-            InputTextPrimary(
-                label = "Email",
+            Spacer(modifier = Modifier.height(20.dp))
+            InputTextDescription(
                 value = input,
-                placeholder = "Masukkan email disini",
+                placeholder = stringResource(id = R.string.text_placeholder_input_description),
                 onChange = { input = it },
-                enabled = false
+                label = stringResource(id = R.string.text_label_input_description),
+                modifier = Modifier.size(
+                    width = 320.dp,
+                    height = 25.dp
+                ),
+                error = true
             )
         }
+
     }
 }
