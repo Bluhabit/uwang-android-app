4623ac31bea109989594830ec4776db4d069a7a9
azkifairuz <azkiajmal@gmail.com>
2023-08-28 19:31:59 +0700

    initial

---
+++ feature/feature-authentication/src/main/java/com/bluehabit/eureka/feature/authentication/editProfile/EditProfileViewModel.kt
@@ -0,0 +1,11 @@
+/*
+ * Copyright © 2023 Blue Habit.
+ *
+ * Unauthorized copying, publishing of this file, via any medium is strictly prohibited
+ * Proprietary and confidential
+ */
+
+package com.bluehabit.eureka.feature.authentication.editProfile
+
+class EditProfileViewModel {
+}
