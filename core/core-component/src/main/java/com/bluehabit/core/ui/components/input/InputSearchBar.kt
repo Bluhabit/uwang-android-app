1db1736e0f2829cd46125574e6e72f25050e45cb
WAHYU <wahyurenaldi27@gmail.com>
2023-09-01 23:06:29 +0700

    Create task search page

--- core/core-component/src/main/java/com/bluehabit/core/ui/components/input/InputSearchBar.kt
+++ core/core-component/src/main/java/com/bluehabit/core/ui/components/input/InputSearchBar.kt
@@ -7,12 +7,7 @@
 
 package com.bluehabit.core.ui.components.input
 
-import androidx.compose.foundation.background
-import androidx.compose.foundation.border
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
@@ -27,10 +22,7 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.shadow
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
@@ -48,30 +40,25 @@ fun SearchBar(
     value: String = String.Empty,
     placeholder: String = String.Empty,
     onChange: (String) -> Unit = {},
+    modifier: Modifier = Modifier,
 ) {
-    Row(
-        horizontalArrangement = Arrangement.spacedBy(8.dp, Alignment.Start),
-        verticalAlignment = Alignment.CenterVertically,
-    ) {
-        Modifier
-            .shadow(elevation = 2.dp, spotColor = Color(0x0D101828), ambientColor = Color(0x0D101828))
-            .border(width = 1.dp, color = Color(0xFFD0D5DD), shape = RoundedCornerShape(size = 8.dp))
-            .width(320.dp)
-            .height(40.dp)
-            .background(color = Color(0xFFFFFFFF), shape = RoundedCornerShape(size = 8.dp))
-            .padding(start = 14.dp, top = 10.dp, end = 14.dp, bottom = 10.dp)
-    }
     OutlinedTextField(
+
         value = value,
         onValueChange = onChange,
         placeholder = {
             Text(
+                modifier = Modifier
+                    .width(228.dp)
+                    .height(20.dp),
                 text = placeholder,
-                style = MaterialTheme.typography.body1,
+                style = MaterialTheme.typography.subtitle1,
                 fontWeight = FontWeight.W400,
-                fontSize = 14.sp,
+                fontSize = 14.sp
             )
         },
+        singleLine = true,
+        shape = RoundedCornerShape(8.dp),
         leadingIcon = {
             Icon(
                 painter = painterResource(id = R.drawable.search),
@@ -80,9 +67,10 @@ fun SearchBar(
                 modifier = Modifier.size(20.dp)
             )
         },
-        modifier = Modifier.fillMaxWidth(),
-
-        )
+        modifier = Modifier
+            .width(310.dp)
+            .height(51.dp)
+    )
 }
 
 
