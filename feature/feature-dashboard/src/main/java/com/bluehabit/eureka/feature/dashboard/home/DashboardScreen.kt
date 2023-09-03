6ca553375580a621f497941fdee5abfbb1e11642
trian damai <triannurizkillah@gmail.com>
2023-09-03 23:11:09 +0700

    feat: integration

--- feature/feature-dashboard/src/main/java/com/bluehabit/eureka/feature/dashboard/home/DashboardScreen.kt
+++ feature/feature-dashboard/src/main/java/com/bluehabit/eureka/feature/dashboard/home/DashboardScreen.kt
@@ -9,19 +9,30 @@ package com.bluehabit.eureka.feature.dashboard.home
 
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.FabPosition
+import androidx.compose.material.FloatingActionButton
+import androidx.compose.material.FloatingActionButtonDefaults
+import androidx.compose.material.Icon
 import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
 import androidx.compose.material.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import app.trian.mvi.Navigation
 import app.trian.mvi.ui.UIWrapper
 import app.trian.mvi.ui.internal.contract.UIContract
 import app.trian.mvi.ui.internal.rememberUIController
+import com.bluehabit.core.ui.R
 import com.bluehabit.core.ui.components.button.ButtonPrimary
+import com.bluehabit.core.ui.components.navigationbar.DashboardBottomNavigation
 import com.bluehabit.core.ui.routes.Routes
 import com.bluehabit.core.ui.theme.GaweanTheme
+import com.bluehabit.core.ui.theme.Primary600
 
 @Navigation(
     route = Routes.Dashboard.routeName,
@@ -46,24 +57,46 @@ fun DashboardScreen(
         }
     })
     Scaffold(
-        topBar = {
-            TopAppBar {
-                Text(text = "Todo")
-            }
-        },
         bottomBar = {
+            DashboardBottomNavigation(
+                currentRoute = state.dashboardScreen,
+                onRefresh = {
 
-        }
+                },
+                onClick = {
+                    commit { copy(dashboardScreen=it.screen) }
+                }
+            )
+        },
+        floatingActionButton = {
+            FloatingActionButton(
+                onClick = { /*TODO*/ },
+                content = {
+                    Icon(
+                        painter = painterResource(id = R.drawable.icon_item_bottom_navigation_user_active),
+                        contentDescription = null,
+                        modifier = Modifier.size(37.dp)
+                    )
+                },
+                backgroundColor = Primary600,
+                contentColor = Color.White,
+                elevation = FloatingActionButtonDefaults.elevation(
+                    defaultElevation = 0.dp,
+                    pressedElevation = 1.dp,
+                    hoveredElevation = 0.dp,
+                    focusedElevation = 0.dp
+                )
+            )
+        },
+        isFloatingActionButtonDocked = true,
+        floatingActionButtonPosition = FabPosition.Center
     ) {
         Column(
             modifier = modifier.padding(it)
         ) {
-            ButtonPrimary(
-                text = "Keluar",
-                onClick = {
-                    dispatch(DashboardAction.SignOut)
-                }
-            )
+            when (state.dashboardScreen) {
+                0 -> {}
+            }
         }
     }
 }
