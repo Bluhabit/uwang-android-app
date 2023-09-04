e45f0c989951a3d4efa18e714e709a11b6f56bdb
trian damai <triannurizkillah@gmail.com>
2023-09-04 20:46:09 +0700

    feat: integration

---
+++ feature/feature-dashboard/src/main/java/com/bluehabit/eureka/feature/dashboard/screen/ProfileScreen.kt
@@ -0,0 +1,89 @@
+/*
+ * Copyright © 2023 Blue Habit.
+ *
+ * Unauthorized copying, publishing of this file, via any medium is strictly prohibited
+ * Proprietary and confidential
+ */
+
+package com.bluehabit.eureka.feature.dashboard.screen
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.material.IconButton
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Tab
+import androidx.compose.material.TabRow
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.bluehabit.core.ui.R
+import com.bluehabit.core.ui.components.input.SearchBar
+import com.bluehabit.core.ui.components.item.itemTask.ItemTask
+import com.bluehabit.core.ui.components.item.itemTask.ProgressItemTask
+import com.bluehabit.core.ui.theme.GaweanTheme
+import com.bluehabit.core.ui.theme.Gray600
+import com.bluehabit.core.ui.theme.Primary700
+import com.bluehabit.eureka.feature.dashboard.DashboardState
+import com.bluehabit.eureka.feature.dashboard.model.ItemTabListTask
+
+@Composable
+fun ProfileScreen(
+    state: DashboardState,
+    onClickNotification:()->Unit={},
+) {
+    LazyColumn(
+        verticalArrangement = Arrangement.spacedBy(20.dp),
+        contentPadding = PaddingValues(top = 12.dp)
+    ) {
+        item {
+            Row(
+                horizontalArrangement = Arrangement.spacedBy(12.dp),
+                verticalAlignment = Alignment.CenterVertically,
+                modifier = Modifier
+                    .padding(horizontal = 20.dp)
+            ) {
+                Text(
+                    text = "Profil",
+                    style = MaterialTheme.typography.body2.copy(
+                        fontWeight = FontWeight.Medium
+                    ),
+                    fontWeight = FontWeight.W500,
+                    color = Gray600
+                )
+                Spacer(modifier = Modifier.weight(1f))
+                IconButton(onClick = onClickNotification) {
+                    Image(
+                        painter = painterResource(id = R.drawable.ic_bell),
+                        contentDescription = stringResource(id = R.string.content_description_logo)
+                    )
+                }
+            }
+        }
+        item {
+
+        }
+    }
+}
+
+@Preview(showBackground = true, backgroundColor = 0xFFFFFFFF)
+@Composable
+fun ProfileScreenPreview() {
+    GaweanTheme {
+        ProfileScreen(DashboardState(fullName = "Olivia Rhye"))
+    }
+}
