1af04d3fa260f210ea324d597f5be70773d1febf
trian damai <triannurizkillah@gmail.com>
2023-09-05 18:27:08 +0700

    feat: integration

--- feature/feature-dashboard/src/main/java/com/bluehabit/eureka/feature/dashboard/screen/ProfileScreen.kt
+++ feature/feature-dashboard/src/main/java/com/bluehabit/eureka/feature/dashboard/screen/ProfileScreen.kt
@@ -8,6 +8,7 @@
 package com.bluehabit.eureka.feature.dashboard.screen
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
@@ -15,8 +16,10 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.material.Icon
 import androidx.compose.material.IconButton
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Tab
@@ -25,6 +28,7 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
@@ -39,6 +43,7 @@ import com.bluehabit.core.ui.components.item.itemTask.ItemTask
 import com.bluehabit.core.ui.components.item.itemTask.ProgressItemTask
 import com.bluehabit.core.ui.theme.GaweanTheme
 import com.bluehabit.core.ui.theme.Gray600
+import com.bluehabit.core.ui.theme.Primary600
 import com.bluehabit.core.ui.theme.Primary700
 import com.bluehabit.eureka.feature.dashboard.DashboardState
 import com.bluehabit.eureka.feature.dashboard.model.ItemTabListTask
@@ -83,6 +88,34 @@ fun ProfileScreen(
                 labels = listOf(),
                 items = listOf()
             )
+        }
+        item {
+            Row(
+                modifier = Modifier
+                    .clickable {  }
+                    .fillMaxWidth()
+                    .padding(start = 20.dp, top = 16.dp, end = 20.dp, bottom = 16.dp),
+                horizontalArrangement = Arrangement.Start,
+                verticalAlignment = Alignment.CenterVertically,
+
+                ) {
+                Text(
+                    text = "",
+                    style = androidx.compose.ui.text.TextStyle(
+                        fontSize = 14.sp,
+                        lineHeight = 24.sp,
+                        fontWeight = FontWeight(400),
+                        color = Color(0xFF101828),
+                    )
+                )
+                Icon(
+                    painter = painterResource(
+                        id = R.drawable.ic_bell
+                    ),
+                    tint = Primary600,
+                    contentDescription = "",
+                )
+            }
         }
         item {
             ButtonLogout()
