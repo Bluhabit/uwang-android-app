d0871921a4b5dd0f4ebe92451593bb25b4b02c8f
WAHYU <wahyurenaldi27@gmail.com>
2023-12-17 22:19:23 +0700

    Integration Sign In Google

--- androidApp/src/main/java/com/bluhabit/blu/android/presentation/authentication/signin/SignInEffect.kt
+++ androidApp/src/main/java/com/bluhabit/blu/android/presentation/authentication/signin/SignInEffect.kt
@@ -7,9 +7,6 @@
 
 package com.bluhabit.blu.android.presentation.authentication.signin
 
-import com.google.android.gms.auth.api.signin.GoogleSignInAccount
-import com.google.android.gms.tasks.Task
-
 sealed interface AuthEffect {
     object Nothing : AuthEffect
     object NavigateToHome : AuthEffect
