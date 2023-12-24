5427f7285f41ce0d77326b4c257916dfd8671dff
fauzangraciaramadhani <fauzanramadhani06@gmail.com>
2023-12-24 14:03:14 +0700

    feat: put all text into the string resources

--- androidApp/src/main/java/com/bluhabit/blu/android/presentation/authentication/signin/screen/OtpSignInScreen.kt
+++ androidApp/src/main/java/com/bluhabit/blu/android/presentation/authentication/signin/screen/OtpSignInScreen.kt
@@ -27,6 +27,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalFocusManager
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.bluehabit.core.ui.R
@@ -72,12 +73,15 @@ fun OtpSignInScreen(
                 .padding(horizontal = 16.dp)
         ) {
             Text(
-                text = "Masukkan Kode OTP",
+                text = stringResource(id = R.string.otp_sign_in_screen_reset_title),
                 style = CustomTypography.Body.XL.W600,
                 color = CustomColor.Neutral.Grey13
             )
             Text(
-                text = "Silakan periksa kotak masuk dan spam pada email J****11@gmail.com, masukkan kode OTP  untuk buat password baru.",
+                text = stringResource(
+                    R.string.otp_sign_in_screen_reset_instruction,
+                    "J****11@gmail.com"
+                ),
                 style = CustomTypography.Body.Small.W400,
                 color = CustomColor.Neutral.Grey9
             )
@@ -90,7 +94,7 @@ fun OtpSignInScreen(
                 .padding(top = 4.dp, bottom = 7.dp)
         ) {
             Text(
-                text = "Waktu konfirmasi ",
+                text = stringResource(id = R.string.otp_sign_in_screen_reset_confirmation_time),
                 style = CustomTypography.Body.XS.W500,
                 color = CustomColor.Neutral.Grey13
             )
@@ -119,7 +123,7 @@ fun OtpSignInScreen(
         }
         Spacer(modifier = Modifier.weight(1f))
         ButtonPrimary(
-            text = "Selanjutnya",
+            text = stringResource(id = R.string.otp_sign_in_screen_reset_next_button_text),
             modifier = Modifier
                 .fillMaxWidth()
                 .padding(start = 16.dp, end = 16.dp, bottom = 42.dp),
