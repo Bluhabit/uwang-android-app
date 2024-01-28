52bf34fe2e3cfd128351c82858bf80732e86bf9d
azkifairuz <azkiajmal@gmail.com>
2024-01-29 00:30:45 +0700

    add asset

--- androidApp/src/main/java/com/bluhabit/blu/android/presentation/authentication/onboard/screen/ScreenFrameOnboarding.kt
+++ androidApp/src/main/java/com/bluhabit/blu/android/presentation/authentication/onboard/screen/ScreenFrameOnboarding.kt
@@ -19,7 +19,12 @@ import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.navigation.compose.rememberNavController
+import com.bluhabit.blu.android.presentation.authentication.onboard.OnboardScreen
+import com.bluhabit.core.ui.theme.UwangTheme
+import kotlinx.coroutines.flow.flowOf
 
 @Composable
 fun ScreenFrameOnboarding(
