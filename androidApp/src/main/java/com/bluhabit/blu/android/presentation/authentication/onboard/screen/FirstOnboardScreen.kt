3b460037d5f2d3177b8ee63ce4fad045e1f484d0
azkifairuz <azkiajmal@gmail.com>
2024-01-30 04:16:28 +0700

    add vector

--- androidApp/src/main/java/com/bluhabit/blu/android/presentation/authentication/onboard/screen/FirstOnboardScreen.kt
+++ androidApp/src/main/java/com/bluhabit/blu/android/presentation/authentication/onboard/screen/FirstOnboardScreen.kt
@@ -8,13 +8,11 @@
 package com.bluhabit.blu.android.presentation.authentication.onboard.screen
 
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -23,7 +21,6 @@ import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import com.bluehabit.core.ui.R
 import com.bluhabit.core.ui.theme.Gray900
 import com.bluhabit.core.ui.theme.UwangDimens
--- androidApp/src/main/java/com/bluhabit/blu/android/presentation/authentication/onboard/screen/SecondOnboardScreen.kt
+++ androidApp/src/main/java/com/bluhabit/blu/android/presentation/authentication/onboard/screen/SecondOnboardScreen.kt
@@ -8,12 +8,9 @@
 package com.bluhabit.blu.android.presentation.authentication.onboard.screen
 
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.RowScope
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
@@ -21,15 +18,12 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.shadow
-import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import com.bluehabit.core.ui.R
 import com.bluhabit.core.ui.theme.UwangDimens
 import com.bluhabit.core.ui.theme.UwangTheme
