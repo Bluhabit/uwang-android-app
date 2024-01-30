37ae5ad0f1a67d406bdaf8cf743f71b563a31d9c
azkifairuz <azkiajmal@gmail.com>
2024-01-30 07:00:29 +0700

    fix hierarki

--- androidApp/src/main/java/com/bluhabit/blu/android/presentation/authentication/onboard/screen/ThirdOnboardScreen.kt
+++ androidApp/src/main/java/com/bluhabit/blu/android/presentation/authentication/onboard/screen/ThirdOnboardScreen.kt
@@ -8,7 +8,6 @@
 package com.bluhabit.blu.android.presentation.authentication.onboard.screen
 
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxHeight
@@ -19,7 +18,6 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
