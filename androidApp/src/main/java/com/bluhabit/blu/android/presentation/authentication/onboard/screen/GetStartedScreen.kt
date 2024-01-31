fab684a836b94248a492b91ddf26a76a2ccd4627
azkifairuz <azkiajmal@gmail.com>
2024-02-01 00:37:27 +0700

    fix unused import

--- androidApp/src/main/java/com/bluhabit/blu/android/presentation/authentication/onboard/screen/GetStartedScreen.kt
+++ androidApp/src/main/java/com/bluhabit/blu/android/presentation/authentication/onboard/screen/GetStartedScreen.kt
@@ -37,7 +37,6 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import com.bluehabit.core.ui.R
 import com.bluhabit.core.ui.components.button.ButtonGoogle
 import com.bluhabit.core.ui.components.button.ButtonPrimary
