a933ba553a2d574083cd5e0489bba4e6c610cc89
trian damai <triannurizkillah@gmail.com>
2024-02-02 21:51:36 +0700

    feat: fix linter

--- core/core-component/src/main/java/com/bluhabit/core/ui/components/textfield/ClickableTextFieldPrimary.kt
+++ core/core-component/src/main/java/com/bluhabit/core/ui/components/textfield/ClickableTextFieldPrimary.kt
@@ -7,43 +7,33 @@
 
 package com.bluhabit.core.ui.components.textfield
 
-import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.foundation.text.KeyboardActions
-import androidx.compose.foundation.text.KeyboardOptions
-import androidx.compose.material.Icon
-import androidx.compose.material.IconButton
 import androidx.compose.material.OutlinedTextField
 import androidx.compose.material.Text
 import androidx.compose.material.TextFieldColors
 import androidx.compose.material.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.focus.FocusManager
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.Shape
-import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.platform.LocalFocusManager
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.unit.dp
-import com.bluehabit.core.ui.R
 import com.bluhabit.core.ui.components.alert.AlertTextFieldError
 import com.bluhabit.core.ui.components.alert.AlertTextFieldSuccess
 import com.bluhabit.core.ui.components.alert.AlertTextFieldWithHint
 import com.bluhabit.core.ui.theme.UwangColors
-import com.bluhabit.core.ui.theme.UwangDimens
 import com.bluhabit.core.ui.theme.UwangTypography
 
 @Composable
 fun ClickableTextFieldPrimary(
     modifier: Modifier = Modifier,
-    label: @Composable (() -> Unit)? = null,
+    label: String = "",
+    placeholder: String = "",
     shape: Shape = RoundedCornerShape(12.dp),
     colors: TextFieldColors = TextFieldDefaults.outlinedTextFieldColors(
         textColor = UwangColors.Text.Main,
@@ -58,56 +48,58 @@ fun ClickableTextFieldPrimary(
     ),
     textStyle: TextStyle = UwangTypography.BodySmall.Regular,
     value: String = "",
-    onValueChange: (String) -> Unit = {},
-    enabled: Boolean = true,
-    isError: Boolean = false,
+    state: TextFieldState = TextFieldState.None,
     leadingIcon: @Composable (() -> Unit)? = null,
     trailingIcon: @Composable (() -> Unit)? = null,
     visualTransformation: VisualTransformation = VisualTransformation.None,
-    keyboardOptions: KeyboardOptions = KeyboardOptions.Default,
-    singleLine: Boolean = true,
-    maxLines: Int = 1,
-    readOnly: Boolean = false,
-    onAction: (FocusManager) -> Unit = {}
+    onClick: () -> Unit = {}
 ) {
-    val focus = LocalFocusManager.current
-    OutlinedTextField(
-        modifier = modifier,
-        placeholder = label,
-        shape = shape,
-        colors = colors,
-        value = value,
-        onValueChange = onValueChange,
-        enabled = enabled,
-        textStyle = textStyle,
-        isError = isError,
-        leadingIcon = leadingIcon,
-        trailingIcon = trailingIcon,
-        visualTransformation = visualTransformation,
-        keyboardOptions = keyboardOptions,
-        keyboardActions = KeyboardActions(
-            onDone = {
-                onAction(focus)
+    Column(
+        verticalArrangement = Arrangement.spacedBy(4.dp),
+    ) {
+        Text(
+            text = label,
+            style = UwangTypography.BodySmall.Regular,
+            color = UwangColors.Palette.Neutral.Grey9
+        )
+        OutlinedTextField(
+            modifier = modifier
+                .clip(shape)
+                .clickable {
+                    onClick()
+                },
+            placeholder = {
+                Text(
+                    text = placeholder,
+                    style = UwangTypography.BodySmall.Regular,
+                    color = UwangColors.Palette.Neutral.Grey7
+                )
             },
-            onGo = {
-                onAction(focus)
-            },
-            onNext = {
-                onAction(focus)
-            },
-            onPrevious = {
-                onAction(focus)
-            },
-            onSend = {
-                onAction(focus)
-            },
-            onSearch = {
-                onAction(focus)
-            },
-        ),
-        singleLine = singleLine,
-        maxLines = maxLines,
-        readOnly = readOnly,
-    )
+            shape = shape,
+            colors = colors,
+            value = value,
+            onValueChange = {},
+            enabled = true,
+            textStyle = textStyle,
+            isError = state is TextFieldState.Error,
+            leadingIcon = leadingIcon,
+            trailingIcon = trailingIcon,
+            visualTransformation = visualTransformation,
+            singleLine = true,
+            maxLines = 1,
+            readOnly = true,
+        )
+        when (state) {
+            is TextFieldState.Error -> AlertTextFieldError(state.errorText)
+            TextFieldState.None -> Unit
+            is TextFieldState.Success -> {
+                AlertTextFieldSuccess(state.successText)
+            }
+
+            is TextFieldState.WithHint -> {
+                AlertTextFieldWithHint(state.hintText)
+            }
+        }
+    }
 }
 
